4819a48f55dfeeb8b00a34ae1868c32ad20e3ffc
Denys Sttrilchuk <denia876@gmail.com>
2024-02-18 16:19:45 +0200

    TS  lesson-2 (problem withb users)

--- src/components/UsersContainer/Users.tsx
+++ src/components/UsersContainer/Users.tsx
@@ -1,10 +1,18 @@
-import {FC, PropsWithChildren} from "react";
+import {FC, PropsWithChildren, useEffect, useState} from "react";
+import {IUser} from "../../interfaces";
+import {userService} from "../../services";
 
 interface IProps extends PropsWithChildren {
 
 }
 
 const Users: FC<IProps> = () => {
+
+    const [users, setUsers] = useState<IUser[]>([]);
+
+    useEffect(() => {
+        userService.getAll().then(({data}) => setUsers(data))
+    }, []);
     return (
         <div>
             Users
