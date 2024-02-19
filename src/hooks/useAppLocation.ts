ad1359fa085aac0e6561d1a025ab19837d4356bb
Denys Sttrilchuk <denia876@gmail.com>
2024-02-19 15:40:04 +0200

    middle

--- src/hooks/useAppLocation.ts
+++ src/hooks/useAppLocation.ts
@@ -1,6 +1,6 @@
-import {useLocation} from "react-router-dom";
+import {useLocation, Location} from "react-router-dom";
 
-const useAppLocation = <T> ():Location<T> => useLocation();
+const useAppLocation = <T>(): Location<T> => useLocation();
 
 export {
     useAppLocation
